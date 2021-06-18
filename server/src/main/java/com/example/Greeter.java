46fb6f66f61ad685d04d6fc2e1077b6b10ad5e99
Fahadi1 <73292833+Fahadi1@users.noreply.github.com>
2021-06-18 11:41:34 +0200

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -1,3 +1,6 @@
+/**
+ * Info about this package example, package-info.java file.
+ */
 package com.example;
 
 public class Greeter {
@@ -11,7 +14,6 @@ public class Greeter {
   /**
   ** this is a method to greet 
   ** @param someone : the someone to be greeted
-  ** @return String : the right format of the returned string
   **/
   public final String greet(final String someone) {
     return String.format("Hello babacar, %s!", someone);
